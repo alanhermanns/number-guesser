597f1f7144583028b1e1654dc3648ad5359166ab
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-10-02 11:24:19 -0700

    test now checks for all possibilities

--- test/example.test.js
+++ test/example.test.js
@@ -1,13 +1,13 @@
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
-import compareNumbers from '../number-checker.js/';
+import compareNumbers from '../number-checker.js';
 
 const test = QUnit.test;
 
 test('should return 0 if the user inputs the randomly generated number', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-    const correctNumber = 5;
+    const correctNumber = Math.ceil(Math.ceil(Math.random() * 100) / 5);
     const userInput = correctNumber;
     //Act 
     // Call the function you're testing and set the result to a const
@@ -15,4 +15,27 @@ test('should return 0 if the user inputs the randomly generated number', functio
     //Assert
     // Make assertions about what is expected valid result
     assert.equal(result, 0);
+});
+test('should return -1 if the user inputs a number less than the randomly generated number', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const correctNumber = Math.ceil(Math.ceil(Math.random() * 100) / 5);
+    const userInput = correctNumber - 1;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = compareNumbers(userInput, correctNumber);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, -1);
+});test('should return 0 if the user inputs a number greater than the randomly generated number', function(assert) {
+    //Arrange
+    // Set up your parameters and expectations
+    const correctNumber = Math.ceil(Math.ceil(Math.random() * 100) / 5);
+    const userInput = correctNumber + 1;
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const result = compareNumbers(userInput, correctNumber);
+    //Assert
+    // Make assertions about what is expected valid result
+    assert.equal(result, 1);
 });
